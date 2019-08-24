fa79efc3dd6bfcc804832ea3626d1a15387a2a7e
Jake Carnevale <jake.carnevale@realtruck.com>
2019-08-24 00:28:05 -0500

    Add tailwindcss and purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,20 @@
+const purgecss = {
+  '@fullhuman/postcss-purgecss': {
+    // Specify the paths to all of the template files in your project
+    content: [
+      './src/**/*.html',
+      './src/**/*.vue',
+    ],
+
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+};
+
 module.exports = {
-  plugins: {
+  plugins: Object.assign({
+    tailwindcss: {},
     autoprefixer: {},
   },
+  process.env.NODE_ENV === 'production' ? purgecss : {}),
 };
