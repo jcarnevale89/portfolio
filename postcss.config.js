5236a92a95c40f56e23f60ea2e619a60325b7da0
Jake Carnevale <jake.carnevale@realtruck.com>
2019-08-28 08:17:38 -0500

    Fix purgecss html file path

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ const purgecss = {
   '@fullhuman/postcss-purgecss': {
     // Specify the paths to all of the template files in your project
     content: [
-      './src/**/*.html',
+      './public/**/*.html',
       './src/**/*.vue',
     ],
 
