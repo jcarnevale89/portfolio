7a5050707437e8314a58f331f40c64054fefe089
Jake Carnevale <jake.carnevale@realtruck.com>
2023-06-02 15:12:35 -0500

    Fix github deploy

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:jcarnevale89/jcarnevale89.github.io.git master
+git push -f git@github.com:jcarnevale89/jcarnevale89.github.io.git main:gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
